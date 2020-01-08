97adf70516cba360ca6fd32da08bf16af2d38392
Matthew <43913445+mdhinkle038@users.noreply.github.com>
2020-01-07 19:04:50 -0500

    Add files via upload

---
+++ UPPS_tool_1.ps1
@@ -0,0 +1,4 @@
+﻿<# Set and encrypt credentials to file using default method #>
+
+$credential = Get-Credential
+$credential.Password | ConvertFrom-SecureString | Set-Content C:\Users\user_name\Documents\Tools\ToolCred.txt
